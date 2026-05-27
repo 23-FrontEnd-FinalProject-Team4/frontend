066dd301da1825a9bdd5129a7dffe690b3d8f9cc
douk9909 <douk9909@gmail.com>
2026-05-27 15:24:53 +0900

    Feat: 프로그레스바 공통 컴포넌트 구현

---
+++ src/components/progressBar/type.ts
@@ -0,0 +1,13 @@
+export type ProgressBarSize = 'sm' | 'md' | 'lg';
+
+export interface ProgressBarProps {
+  value?: number;
+  max?: number;
+  completed?: number;
+  total?: number;
+  size?: ProgressBarSize;
+  showTrackPattern?: boolean;
+  className?: string;
+  barClassName?: string;
+  label?: string;
+}
