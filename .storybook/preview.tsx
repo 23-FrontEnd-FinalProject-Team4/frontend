903ec81016c5ff99f2e16b073ffa0096be2065b6
isuzzi <isuzzi11@gmail.com>
2026-05-30 01:45:08 +0900

    Feat: storybook 모바일, 태블릿, 데스크톱 viewport 추가

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -9,6 +9,30 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    viewport: {
+      viewports: {
+        mobile: {
+          name: 'Mobile',
+          styles: {
+            width: '375px',
+          },
+        },
+
+        tablet: {
+          name: 'Tablet',
+          styles: {
+            width: '768px',
+          },
+        },
+
+        desktop: {
+          name: 'Desktop',
+          styles: {
+            width: '1280px',
+          },
+        },
+      },
+    },
 
     a11y: {
       // 'todo' - show a11y violations in the test UI only
