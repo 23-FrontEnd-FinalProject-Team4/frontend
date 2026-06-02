47b9c7683efb0db396c5097bbf77e7279a64d919
Evo1ver <62330567+Evo1ver@users.noreply.github.com>
2026-06-02 14:39:07 +0900

    refactor: 공백 오류 수정

--- src/utils/date/date.test.ts
+++ src/utils/date/date.test.ts
@@ -7,24 +7,28 @@ describe('date', () => {
       // Arrange
       const interval = 30;
       const expected = [0, 30];
-  
+
       // Act
       const result = generateMinutesByInterval(interval);
-  
+
       // Assert
       expect(result).toEqual(expected);
     });
 
-    it("인수 없는 경우 테스트", () => {
+    it('인수 없는 경우 테스트', () => {
       // Arrange
-      const expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59];
+      const expected = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+        25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+        48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+      ];
 
       // Act
       const result = generateMinutesByInterval();
 
       // Assert
       expect(result).toEqual(expected);
-    })
+    });
   });
 
   describe('generateHoursByInterval', () => {
@@ -43,7 +47,7 @@ describe('date', () => {
     it('인수 없는 경우 테스트', () => {
       // Arrange
       const expected = [
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
       ];
 
       // Act
