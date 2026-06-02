6eba2d0200071b4716ec12abec40e399189253ef
Evo1ver <62330567+Evo1ver@users.noreply.github.com>
2026-06-02 15:15:26 +0900

    fix: minute 선택 부분 정렬 수정

--- src/components/calendar/time/CalendarTime.tsx
+++ src/components/calendar/time/CalendarTime.tsx
@@ -31,7 +31,7 @@ const CalendarTime = ({
       </div>
 
       {/* Minute */}
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col justify-center gap-2">
         {minutes.map((minute) => {
           return (
             <Button
