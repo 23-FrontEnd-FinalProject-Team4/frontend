a7f1f69e45fd97f6fe32211115e155609f8aa2ea
isuzzi <isuzzi11@gmail.com>
2026-06-04 14:36:27 +0900

    Feat: article 페이지 검색 input 구현

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,8 +1,24 @@
-export default function ArticlesPage() {
+import SearchIcon from '@/components/icons/search.svg?react';
+
+import type { ArticlesPageProps } from './type';
+
+export default function ArticlesPage({ value, onChange, placeholder }: ArticlesPageProps) {
   return (
     <div className="flex h-screen">
       <main className="">
-        <div className="mx-auto p-8">ArticlesPage</div>
+        <div className="mx-auto p-8">
+          <h1>자유게시판</h1>
+          <div className="relative">
+            <SearchIcon className="absolute top-1/2 left-4 -translate-y-1/2" />
+
+            <input
+              value={value}
+              onChange={onChange}
+              placeholder={placeholder}
+              className="border-brand-primary h-12 w-full rounded-full border pr-4 pl-12 outline-none"
+            />
+          </div>
+        </div>
       </main>
     </div>
   );
---
+++ src/app/articles/type.ts
@@ -0,0 +1,5 @@
+export type ArticlesPageProps = {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+};
