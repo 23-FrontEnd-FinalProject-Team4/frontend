7988ff3198ec498ac1b700cab23d2c41cf9a4a49
chahyunlee <chahyun023@gmail.com>
2026-06-08 14:25:40 +0900

    refactor: 계정 설정 페이지 구조를 컴포넌트 단위로 정리

---
+++ src/app/settings/_components/SettingProfileFormSection.tsx
@@ -0,0 +1,38 @@
+import SecessionIcon from '@/assets/icons/secession.svg';
+
+import EditableProfileImage from '@/components/editableProfileImage/EditableProfileImage';
+import Input from '@/components/input/Input';
+
+export default function SettingProfileFormSection() {
+  return (
+    <main className="bg-background-primary flex max-h-[800px] w-full max-w-[780px] flex-col rounded-2xl p-8 md:p-18">
+      <h1 className="text-medium font-semibold">계정 설정</h1>
+      <div className="my-8 flex justify-center">
+        <EditableProfileImage src="/images/default-profile.png" size="lg" />
+      </div>
+      <form className="flex flex-col gap-4">
+        <div className="flex flex-col gap-1.5">
+          <label htmlFor="name">이름</label>
+          <Input id="name" defaultValue="안해나" placeholder="이름을 입력해주세요." />
+        </div>
+        <div className="flex flex-col gap-1.5">
+          <label htmlFor="email">이메일</label>
+          <Input
+            id="email"
+            defaultValue="codeit@codeit.com"
+            placeholder="이메일을 입력해주세요."
+            isDisabled
+          />
+        </div>
+        <div className="flex flex-col gap-1.5">
+          <label htmlFor="password">비밀번호</label>
+          <Input id="password" value="dddd" isDisabled rightButtonText="변경하기" />
+        </div>
+        <button type="button" className="text-status-danger mt-2 flex items-center gap-1">
+          <SecessionIcon />
+          <span>회원 탈퇴하기</span>
+        </button>
+      </form>
+    </main>
+  );
+}
