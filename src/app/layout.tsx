8b57e1c756a5286f76ca55f41706a7e4de0d7267
isuzzi <isuzzi11@gmail.com>
2026-06-08 12:50:14 +0900

    Feat: appshell 컴포넌트 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,30 @@ import type { Metadata } from 'next';
 
 import '@/app/globals.css';
 
-import AppShell from '@/components/AppShell';
+import Sidebar from '@/components/sideBar/SideBar';
 
 export const metadata: Metadata = {
   title: 'Coworkers',
   description: 'Coworkers project',
 };
 
+// 리스트를 보여주기 위한 데이터
+const SIDEBAR_GROUPS = [
+  { id: 1, name: '경영관리팀', route: '/management' },
+  { id: 2, name: '프로덕트팀', route: '/product' },
+  { id: 3, name: '마케팅팀', route: '/marketing' },
+  { id: 4, name: '콘텐츠팀', route: '/content' },
+];
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body>
-        <AppShell>{children}</AppShell>
+        <div className="bg-background-secondary flex min-h-screen">
+          <Sidebar isLoggedIn={true} groups={SIDEBAR_GROUPS} />
+
+          <main className="flex-1 pt-16 md:pt-0">{children}</main>
+        </div>
       </body>
     </html>
   );
