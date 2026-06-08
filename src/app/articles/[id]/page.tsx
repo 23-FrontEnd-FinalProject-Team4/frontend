62fd42a2ca1ca3c9f8349c3acb92d77670e2c39b
isuzzi <isuzzi11@gmail.com>
2026-06-08 14:56:48 +0900

    Feat: 게시글 상세에서 목록으로 가는 기능 구현

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,5 +1,8 @@
+import Link from 'next/link';
+
 import { mockArticles } from '@/app/articles/mockArticles';
 import { mockComments } from '@/app/articles/mockArticles';
+import ArrowLeft from '@/assets/icons/arrow_left.svg';
 
 import ArticleContent from '@/components/articlesDetail/ArticleContent';
 import ArticleHeader from '@/components/articlesDetail/ArticleHeader';
@@ -12,6 +15,11 @@ const ArticleDetailPage = () => {
   return (
     <div className="mx-auto flex min-h-screen px-4 pt-5 md:p-22">
       <main className="min-h-screen w-full">
+        {/* TODOL: 게시글 목록, 이전글, 다음글 */}
+        <Link href="/articles" className="text-text-default mb-4 flex items-center gap-1">
+          <ArrowLeft className="mb-0.25 h-4 w-4" />
+          <span className="text-md">게시판 목록</span>
+        </Link>
         <div className="bg-background-primary mx-auto flex w-full flex-col gap-4 rounded-2xl px-5 py-10 md:px-10 md:py-14">
           <ArticleHeader
             title={articles.title}
