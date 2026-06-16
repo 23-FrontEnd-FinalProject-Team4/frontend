f38a720418d41737d4be6c10b8411ca849d566f4
isuzzi <isuzzi11@gmail.com>
2026-06-16 17:15:07 +0900

    Feat: 목록으로 버튼 추가

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -6,7 +6,6 @@ import CommentSection from '@/app/articles/_components/articlesDetail/CommentSec
 import ArticleContent from '@/app/articles/_components/articlesDetail/Content';
 import ArticleHeader from '@/app/articles/_components/articlesDetail/Header';
 import LikeButton from '@/app/articles/_components/articlesDetail/LikeButton';
-import ArrowLeft from '@/assets/icons/arrow_left.svg';
 import { formatDate } from '@/utils/formatDate';
 
 const ArticleDetailPage = async ({ params }: { params: Promise<{ id: string }> }) => {
@@ -17,11 +16,6 @@ const ArticleDetailPage = async ({ params }: { params: Promise<{ id: string }> }
   return (
     <div className="mx-auto flex min-h-screen px-4 pt-5 md:p-22">
       <main className="min-h-screen w-full">
-        {/* TODO: 게시글 목록, 이전글, 다음글 */}
-        <Link href="/articles" className="text-text-default mb-4 flex items-center gap-1">
-          <ArrowLeft className="mb-0.25 h-4 w-4" />
-          <span className="text-md">게시판 목록</span>
-        </Link>
         <div className="bg-background-primary mx-auto flex w-full flex-col gap-4 rounded-2xl px-5 py-10 md:px-10 md:py-14">
           <ArticleHeader
             id={`${article.id}`}
@@ -36,6 +30,12 @@ const ArticleDetailPage = async ({ params }: { params: Promise<{ id: string }> }
             initialLikeCount={article.likeCount}
           />
           <CommentSection articleId={`${article.id}`} comments={comments.list} />
+        </div>
+
+        <div className="mt-5 flex w-full justify-center pr-2">
+          <Link href="/articles" className="text-text-default">
+            <span className="text-lg">목록으로</span>
+          </Link>
         </div>
       </main>
     </div>
