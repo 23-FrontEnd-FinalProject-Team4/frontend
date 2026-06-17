3398eae31cf70adfa9d53ed5563ac38d7cbfd7e0
douk9909 <douk9909@gmail.com>
2026-06-17 15:18:55 +0900

    Fix: 팀 수정 페이지 이동 경로 수정

--- src/app/[teamId]/_components/TeamPageClient.tsx
+++ src/app/[teamId]/_components/TeamPageClient.tsx
@@ -156,7 +156,7 @@ const TeamPageClient = ({ teamId, initialDate }: TeamPageClientProps) => {
     }
 
     setIsTeamMenuOpen(false);
-    router.push(`/${selectedGroupId}/edit`);
+    router.push(`/${selectedGroupId}/editteam`);
   };
 
   const openDeleteTeamModal = () => {
