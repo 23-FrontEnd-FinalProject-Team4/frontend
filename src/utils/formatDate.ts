bc3711f46c073cef4b203e18f13baeab3e8b253c
isuzzi <isuzzi11@gmail.com>
2026-06-20 00:33:20 +0900

    Fix: 날짜 표시 오류 수정

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -7,7 +7,9 @@ export const formatDate = (input: DateInput) => {
     return '';
   }
 
-  const [year, month, day] = date.toISOString().slice(0, 10).split('-');
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
 
   return `${year}. ${month}. ${day}`;
 };
