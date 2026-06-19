00f670f25b9d375162d6134589afd130a4e35668
isuzzi <isuzzi11@gmail.com>
2026-06-19 16:23:24 +0900

    Fix: 유저 로그인 상태 수정

--- src/components/sideBar/LoggedInFooter.tsx
+++ src/components/sideBar/LoggedInFooter.tsx
@@ -1,20 +1,86 @@
+'use client';
+import { useState } from 'react';
+
+import { useRouter } from 'next/navigation';
+
+import { User } from '@/apis/auth/type';
+import { logout } from '@/app/_actions/logout.action';
 import ProfileIcon from '@/assets/icons/profile.svg?react';
 import SettingsIcon from '@/assets/icons/setting.svg?react';
+import Dropdown from '@/components/dropdown/Dropdown';
 
-export default function LoggedInFooter({ collapsed }: { collapsed: boolean }) {
+export const openProfileMenu = () => {
+  return [
+    { label: '마이 히스토리', value: 'history' },
+    { label: '계정 설정', value: 'account' },
+    { label: '팀 참여', value: 'team' },
+    { label: '로그아웃', value: 'logout' },
+  ];
+};
+
+export const handleOpenProfileMenu = async (
+  value: string,
+  router: ReturnType<typeof useRouter>,
+) => {
+  switch (value) {
+    case 'history':
+      router.push('/my-history');
+      break;
+    case 'account':
+      router.push('/settings');
+      break;
+    case 'team':
+      router.push('/addteam');
+      break;
+    case 'logout':
+      await logout();
+      router.replace('/login');
+      break;
+  }
+};
+
+export default function LoggedInFooter({
+  isLoggedIn,
+  collapsed,
+  user,
+}: {
+  isLoggedIn: boolean;
+  collapsed: boolean;
+  user: User;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = () => setIsOpen(!isOpen);
+  const router = useRouter();
+
   return (
-    <button
-      type="button"
-      className={`flex w-full items-center border-t border-[#E2E8F0] pt-6 ${
+    <div
+      className={`relative flex w-full items-center border-t border-[#E2E8F0] pt-6 ${
         collapsed ? 'justify-center' : 'justify-between'
       }`}
     >
       <div className="flex flex-row items-center gap-3">
         {/* 사용자 프로필 출력 */}
         <ProfileIcon className="h-10 w-10" />
-        {!collapsed && <span>안해나</span>}
+        {!collapsed && <span>{user.nickname}</span>}
       </div>
-      {!collapsed && <SettingsIcon className="h-6 w-6" />}
-    </button>
+
+      {!collapsed && isLoggedIn && (
+        <div className="relative">
+          <SettingsIcon className="h-6 w-6 cursor-pointer" onClick={toggleOpen} />
+
+          {isOpen && (
+            <div className="absolute right-30 bottom-50 z-20 mb-2">
+              <Dropdown
+                options={openProfileMenu()}
+                onSelect={async (value) => {
+                  await handleOpenProfileMenu(value, router);
+                }}
+                onClose={toggleOpen}
+              />
+            </div>
+          )}
+        </div>
+      )}
+    </div>
   );
 }
--- src/components/sideBar/MobileHeader.tsx
+++ src/components/sideBar/MobileHeader.tsx
@@ -1,20 +1,28 @@
 'use client';
 
+import { useState } from 'react';
+
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 import MenuIcon from '@/assets/icons/gnb_menu.svg?react';
 import LogoLarge from '@/assets/icons/logo_large.svg?react';
 import LogoSmall from '@/assets/icons/logo_small.svg?react';
 import ProfileIcon from '@/assets/icons/profile.svg?react';
+import { handleOpenProfileMenu, openProfileMenu } from '@/components/sideBar/LoggedInFooter';
+import type { MobileHeaderProps } from '@/components/sideBar/type';
 import { cn } from '@/utils/cn';
 
-import type { MobileHeaderProps } from '@/components/sideBar/type';
+import Dropdown from '../dropdown/Dropdown';
 
-export default function MobileHeader({ isLoggedIn, onOpenSideBar }: MobileHeaderProps) {
+export default function MobileHeader({ isLoggedIn, user, onOpenSideBar }: MobileHeaderProps) {
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = () => setIsOpen(!isOpen);
   return (
     <header
       className={cn(
-        'bg-background-primary border-brand-secondary fixed flex h-16 w-full items-center justify-between border px-5 md:hidden',
+        'bg-background-primary border-brand-secondary fixed z-20 flex h-16 w-full items-center justify-between border px-5 md:hidden',
       )}
     >
       <div className={cn('flex items-center gap-4')}>
@@ -36,10 +44,25 @@ export default function MobileHeader({ isLoggedIn, onOpenSideBar }: MobileHeader
         )}
       </div>
 
-      {isLoggedIn ? (
-        <button type="button">
-          <ProfileIcon className="h-8 w-8" />
-        </button>
+      {user ? (
+        <div className="relative z-10">
+          <button type="button" onClick={toggleOpen}>
+            <ProfileIcon className="h-8 w-8" />
+          </button>
+
+          {isOpen && (
+            <div className="absolute right-20 bottom-0">
+              <Dropdown
+                size="sm"
+                options={openProfileMenu()}
+                onSelect={async (value) => {
+                  await handleOpenProfileMenu(value, router);
+                }}
+                onClose={toggleOpen}
+              />
+            </div>
+          )}
+        </div>
       ) : (
         <Link href="/login" className={cn('text-brand-tertiary text-md')}>
           로그인
